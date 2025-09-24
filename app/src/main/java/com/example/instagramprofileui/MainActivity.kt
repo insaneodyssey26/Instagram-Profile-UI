9ecdf11e4da6ce0d1b6d4f0d5aa71d57b69d257d
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-24 16:15:37 +0530

    Username and dropdown

--- app/src/main/java/com/example/instagramprofileui/MainActivity.kt
+++ app/src/main/java/com/example/instagramprofileui/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.instagramprofileui.ui.theme.InstagramProfileUITheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +29,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            TopSection()
         }
+    }
+}
+
+@Composable
+fun TopSection (
+    modifier: Modifier = Modifier
+) {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 40.dp, horizontal = 20.dp),
+    ){
+        Text(
+            text = "insane_odyssey",
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp,
+        )
+        Icon(Icons.Default.KeyboardArrowDown, contentDescription = "Dropdown")
     }
 }
