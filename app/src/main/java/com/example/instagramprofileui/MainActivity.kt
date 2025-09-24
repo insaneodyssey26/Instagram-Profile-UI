34bd4aeb76fbad9e417859a790637316e91648b4
insaneodyssey26 <hydrapikachu29@gmail.com>
2025-09-24 18:00:00 +0530

    Profile photo and name

--- app/src/main/java/com/example/instagramprofileui/MainActivity.kt
+++ app/src/main/java/com/example/instagramprofileui/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowDown
@@ -19,7 +23,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -27,13 +35,17 @@ import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.instagramprofileui.ui.theme.InstagramProfileUITheme
+import java.security.spec.EllipticCurve
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TopSection()
+            Column {
+                TopSection()
+                ProfileInfo()
+            }
         }
     }
 }
@@ -46,22 +58,50 @@ fun TopSection (
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 40.dp, horizontal = 20.dp),
-    ){
-        Text(
-            text = "insane_odyssey",
-            fontWeight = FontWeight.Bold,
-            fontSize = 22.sp,
-        )
-        Icon(Icons.Default.KeyboardArrowDown, contentDescription = "Dropdown")
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row {
+            Text(
+                text = "insane.odyssey",
+                fontWeight = FontWeight.Bold,
+                fontSize = 22.sp,
+                modifier = Modifier.padding(end = 5.dp)
+            )
+            Icon(Icons.Default.KeyboardArrowDown, contentDescription = "Dropdown")
+        }
+        Row {
+            Icon(Icons.Default.Add, contentDescription = "Add")
+            Spacer(modifier = Modifier.width(10.dp))
+            Icon(Icons.Default.Menu, contentDescription = "Menu")
+        }
     }
+}
+
+@Composable
+fun ProfileInfo (
+    modifier: Modifier = Modifier
+) {
     Row (
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 40.dp, horizontal = 20.dp),
-        horizontalArrangement = Arrangement.End
-    ) {
-        Icon(Icons.Default.Add, contentDescription = "Add")
-        Spacer(modifier = Modifier.width(10.dp))
-        Icon(Icons.Default.Menu, contentDescription = "Menu")
+            .padding(horizontal = 20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Image(
+            painter = painterResource(R.drawable.me),
+            contentDescription = "Profile Image",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(22.dp))
+        Text(
+            text = "Masum Ali",
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            modifier = Modifier
+                .padding(bottom = 70.dp)
+        )
     }
+
 }
